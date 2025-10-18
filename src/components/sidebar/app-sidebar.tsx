e1ccadaf6eaa4f50bd787722e7c86413ee60da3e
MertAliuynk <mertuynk3@gmail.com>
2025-10-18 09:32:48 +0300

    a

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -1,23 +1,21 @@
 'use client';
 
-import { Sidebar, SidebarContent, SidebarHeader, SidebarMenu, SidebarMenuItem, SidebarMenuButton } from '../ui/sidebar';
 import { NavigationMenu } from '../layout/navigation';
 
 export function AppSidebar() {
   return (
-    <Sidebar className="hidden md:flex">
-      <SidebarHeader className="p-6">
+    <aside className="hidden md:flex flex-col w-64 bg-white border-r">
+      <div className="p-6 border-b">
         <h1 className="text-xl font-bold text-gray-900">
-          Arya Taksis
+          Arya Taksi
         </h1>
         <p className="text-sm text-gray-600">
           Yoklama Sistemi
         </p>
-      </SidebarHeader>
-      
-      <SidebarContent className="px-4">
+      </div>
+      <div className="px-4">
         <NavigationMenu />
-      </SidebarContent>
-    </Sidebar>
+      </div>
+    </aside>
   );
 }
